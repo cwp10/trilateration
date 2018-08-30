b0ea32e282a850a0c09359e6ed54aadd54f07aa4
WonPyoung Choi <cwp10@msn.com>
2018-08-30 17:08:57 +0900

    자이로 센서와 나침반을 이용하여 AR 구현

---
+++ Assets/Scripts/GyroInput.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+public class GyroInput : MonoBehaviour
+{
+	private bool _gyroEnabled = false;
+	private Gyroscope _gyro = null;
+
+	private GameObject _cameraContainer = null;
+	private Quaternion _rot = Quaternion.identity;
+
+	private void Start()
+	{
+		_cameraContainer = new GameObject("Camera Container");
+		_cameraContainer.transform.position = transform.position;
+		transform.SetParent(_cameraContainer.transform);
+
+		_gyroEnabled = EnableGyro();
+	}
+
+	private bool EnableGyro()
+	{
+		if(SystemInfo.supportsGyroscope)
+		{
+			_gyro = Input.gyro;
+			_gyro.enabled = true;
+
+			_cameraContainer.transform.rotation = Quaternion.Euler(90f, 90f, 0f);
+			_rot = new Quaternion(0, 0, 1, 0);
+			return true;
+		}
+		return false;
+	}
+
+	private void Update()
+	{
+		if(_gyroEnabled)
+		{
+			CompassInterpolation();
+			transform.localRotation = _gyro.attitude * _rot;
+		}
+	}
+
+	private void CompassInterpolation()
+	{
+		Vector3 rot = this.transform.parent.localEulerAngles;
+		rot.y = Input.compass.magneticHeading;
+		this.transform.parent.localRotation = Quaternion.Lerp(this.transform.parent.localRotation, Quaternion.Euler(rot), Time.deltaTime * 5f);
+	}
+}
---
+++ Assets/Scripts/WebCamera.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WebCamera : MonoBehaviour 
+{
+	private bool _camAvailable = false;
+	private WebCamTexture _backCam = null;
+	private Texture _defaultBackground = null;
+
+	public RawImage background = null;
+	public AspectRatioFitter fit = null;
+
+	private void Start()
+	{
+		_defaultBackground = background.texture;
+		WebCamDevice[] devices = WebCamTexture.devices;
+
+		if(devices.Length == 0)
+		{
+			_camAvailable = false;
+			return;
+		}
+
+		for(int i = 0; i < devices.Length; i++)
+		{
+			if(!devices[i].isFrontFacing)
+			{
+				_backCam = new WebCamTexture(devices[i].name, Screen.width, Screen.height);
+			}
+		}
+
+		if(_backCam == null)
+		{
+			return;
+		}
+
+		_backCam.Play();
+		background.texture = _backCam;
+
+		_camAvailable = true;
+	}
+	
+	private void Update()
+	{
+		if(!_camAvailable) return;
+
+		float ratio = (float)_backCam.width / (float)_backCam.height;
+		fit.aspectRatio = ratio;
+
+		float scaleY = _backCam.videoVerticallyMirrored ? -1f : 1f;
+		background.rectTransform.localScale = new Vector3(1f, scaleY, 1f);
+
+		int orient = -_backCam.videoRotationAngle;
+		background.rectTransform.localEulerAngles = new Vector3(0, 0, orient);
+	}
+}
